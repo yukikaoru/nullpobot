386017782726e2507a8b3db444d157f36818cd00
Yuu Shimizu <shimizu@shimizu-macbook.local>
2013-10-07 16:01:14 +0900

    Modify to load settings from a passed yaml file

--- start_nullpo.rb
+++ start_nullpo.rb
@@ -1,36 +1,66 @@
+require 'yaml'
 require 'socket'
 require 'nkf'
 require_relative 'irc_client'
 require_relative 'nullpo_bot'
-server = {
-  host: '127.0.0.1',
-  port: 6667,
-  line_separator: "\r\n",
-}
-local = {
-  user: 'nullpo-bot',
-  host: IPSocket::getaddress(Socket::gethostname),
-  server: IPSocket::getaddress(Socket::gethostname),
-  realname: 'nullpo',
-  nickname: 'nullpo-bot',
-  password: 'your password',
-}
-channel = {
-  name: '#channel',
+
+def _symbolize_keys(value)
+  case value
+  when Hash
+    value.reduce({}) {|result, (key, value)|
+      result.merge({key.is_a?(String) ? key.to_sym : key => _symbolize_keys(value)})
+    }
+  when Enumerable
+    value.map {|element| _symbolize_keys(element)}
+  else
+    value
+  end
+end
+
+def symbolize_keys(hash)
+  _symbolize_keys(hash)
+end
+
+defaults = {
+  server: {
+    host: '127.0.0.1',
+    port: 6667,
+    line_separator: "\r\n",
+  },
+  local: {
+    user: 'nullpo-bot',
+    host: IPSocket::getaddress(Socket::gethostname),
+    server: IPSocket::getaddress(Socket::gethostname),
+    realname: 'nullpo-bot',
+    nickname: 'nullpo-bot',
+    password: 'your password',
+  },
+  channel: {
+    name: '#channel',
+  }
 }
 
-client = IRCClient.new(TCPSocket.open(server[:host], server[:port]), server[:line_separator])
+if ARGV[0]
+   config = symbolize_keys(YAML::load(File.read(ARGV[0])))
+   [:server, :local, :channel].each {|key|
+     config[key] = config[key] ? defaults[key].merge(config[key]) : defaults[key]
+   }
+end
+
+client = IRCClient.new(TCPSocket.open(config[:server][:host], config[:server][:port]), config[:server][:line_separator])
+
 class << client
   def message(command, *params)
     puts '>> ' + IRCMessage.new(nil, command.to_s.upcase, params).to_s
     super
   end
 end
+
 begin
-  client.pass local[:password]
-  client.user local[:user], local[:host], local[:server], local[:realname]
-  client.nick local[:nickname]
-  client.join channel[:name]
+  client.pass config[:local][:password]
+  client.user config[:local][:user], config[:local][:host], config[:local][:server], config[:local][:realname]
+  client.nick config[:local][:nickname]
+  client.join config[:channel][:name]
   bot = NullpoBot.new(client)
   begin
     bot.start
